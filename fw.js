24175b8b26e0fd2c110c5ef1f11cc55262e47718
Zoe <zhiyipan@umich.edu>
2021-07-20 22:17:52 +0800

    zxqfw

--- fw.js
+++ fw.js
@@ -1,4 +1,5 @@
 var distance = 85;
+var deg= 0;
 $(document).ready(function (){
     document.getElementById("closer").onclick=(function(){
         distance+= 5;
@@ -8,5 +9,26 @@ $(document).ready(function (){
     document.getElementById("away").onclick=(function(){
         distance-= 5;
         document.getElementById("image-line").style.width = distance + "px";
+    });
+
+    document.getElementById("refresh").onclick=(function(){
+        distance= 85;
+        document.getElementById("image-line").style.width = distance + "px";
+        var div = document.getElementById('image-line');
+        div.style.webkitTransform = 'translate(-50%,-50%)';
+        div.style.mozTransform    = 'translate(-50%,-50%)';
+        div.style.msTransform     = 'translate(-50%,-50%)';
+        div.style.oTransform      = 'translate(-50%,-50%)';
+        div.style.transform       = 'translate(-50%,-50%)'; 
+    });
+    document.getElementById('next').onclick = (function() {
+        var div = document.getElementById('image-line');
+        deg += 30;
+    
+        div.style.webkitTransform = 'translate(-50%,-50%) rotate('+deg+'deg)'; 
+        div.style.mozTransform    = 'translate(-50%,-50%) rotate('+deg+'deg)'; 
+        div.style.msTransform     = 'translate(-50%,-50%) rotate('+deg+'deg)'; 
+        div.style.oTransform      = 'translate(-50%,-50%) rotate('+deg+'deg)'; 
+        div.style.transform       = 'translate(-50%,-50%) rotate('+deg+'deg)'; 
     });
 })
